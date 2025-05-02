6271d3e45cabd6162e6df8cf7c6b4d6249cce8de
Guntur Adil Abdinegoro <gunturadil.ga@gmail.com>
2025-05-03 01:38:40 +0700

    CORS

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,13 @@ from app.apis.auth_api import auth_bp  # blueprint sudah siap saat ini
 from app.apis.category_api import category_bp
 from app.apis.product_api import product_bp
 from app.apis.specification_api import specification_bp
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     db.init_app(app)
     migrate.init_app(app, db)
